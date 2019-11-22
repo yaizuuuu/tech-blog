ac5d4894a4d116e91a81487cf8a5e274bc6a5f44
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2019-11-23 00:20:14 +0900

    サインイン用のコンポーネント

--- components/organisms/SignInFormCard.vue
+++ components/organisms/SignInFormCard.vue
@@ -1,16 +1,16 @@
 <template>
   <HeaderAndActionsInCard>
     <template v-slot:headerText>
-      Login form
+      Sign in form
     </template>
 
     <template v-slot:cardBody>
-      <LoginForm :user.sync="userComputed" />
+      <AuthForm :user.sync="userComputed" />
     </template>
 
     <template v-slot:actions>
       <v-btn color="primary" @click="signIn">
-        Login
+        Sign in
       </v-btn>
     </template>
   </HeaderAndActionsInCard>
@@ -21,15 +21,15 @@ import { Vue, Component } from 'vue-property-decorator'
 import { Auth } from 'aws-amplify'
 import { UserInterface, User } from '@/domains/User'
 import HeaderAndActionsInCard from '@/components/molecules/HeaderAndActionsInCard'
-import LoginForm from '@/components/molecules/LoginForm'
+import AuthForm from '@/components/molecules/AuthForm'
 
 @Component({
   components: {
     HeaderAndActionsInCard,
-    LoginForm
+    AuthForm
   }
 })
-export default class LoginFormCard extends Vue {
+export default class SignInFormCard extends Vue {
   user: UserInterface = {
     userName: '',
     password: ''
@@ -52,6 +52,7 @@ export default class LoginFormCard extends Vue {
     await user.signIn(Auth)
       .then((success) => {
         console.log(success)
+        this.$router.push('/')
       })
       .catch((fail) => {
         console.log(fail)
